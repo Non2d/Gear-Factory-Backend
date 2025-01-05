9c1b7ad2461ec37ec65276a22265a7e8c0d12960
onedanijo11@gmail.com <onedanijo11@gmail.comy>
2025-01-06 03:09:48 +0900

    Update CORS middleware to allow all origins

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,21 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
 from routers import api
 
 app = FastAPI(docs_url="/docs", openapi_url="/openapi.json")
 
-ALLOWED_ORIGIN = os.getenv("ALLOWED_ORIGIN")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ALLOWED_ORIGIN],
+    allow_origins=["*"],
     allow_credentials=True,   
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"]      
 )
 
 @app.get("/")
